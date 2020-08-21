9da1d68e82f8c4addf668976120179a1f0b24eff
unknown <hussainabutt@gmail.com>
2020-08-21 19:49:52 -0400

    finished all this stuff brody

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//It inherits the properties of the variables on the parent function.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(param1){
+  let counter = 0;
+  for(i = 1;i<=param1;i++){
+    counter += i;
+  }
+  return counter;
+}
+
+console.log(summation(4));
